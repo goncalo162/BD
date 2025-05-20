68ea50cec83f70e800f32a08800681b1a842fab0
Waheed <a106914@uminho.pt>
2025-05-20 22:16:13 +0100

    Gestor Filial Conclusão

--- Populacao.sql
+++ Populacao.sql
@@ -1,6 +1,9 @@
 USE AutoArmando;
 
-INSERT INTO Loja VALUES
+-- DELETE FROM Loja WHERE email = 'autoarmando_montalegre@gmail.com'
+-- DELETE FROM Loja WHERE email = 'autoarmando_braga@gmail.com'
+-- DELETE FROM Loja WHERE email = 'autoarmando_viana_do_castelo@gmail.com'
+INSERT INTO Loja(email, cidade, rua, numero_porta, codigo_postal) VALUES
 ('autoarmando_montalegre@gmail.com', 'Montalegre', 'Rua do Monte', 12, '5470-202'),
 ('autoarmando_braga@gmail.com', 'Braga', 'Rua da Quinta da Armada', 67, '4700-162'),
 ('autoarmando_viana_do_castelo@gmail.com', 'Viana do Castelo', 'Travessa de Viana', 22, '4900-580');
--- UtilizadoresPermissoes.sql
+++ UtilizadoresPermissoes.sql
@@ -54,57 +54,125 @@ GRANT Administrador TO 'firmino_coelho'@'localhost';
 -- DROP ROLE GestorFilial
 CREATE ROLE GestorFilial;
 
--- DROP TABLE GestorFilialLoja
-CREATE TABLE GestorFilialLoja
+-- DROP TABLE AuxTGestorFilialLoja
+CREATE TABLE AuxTGestorFilialLoja
 (
   nome_completo VARCHAR(100) PRIMARY KEY NOT NULL,
   id_loja       INT NOT NULL
 );
 
--- REVOKE SELECT, INSERT, DELETE, UPDATE ON GestorFilialLoja FROM Administrador;
-GRANT SELECT, INSERT, DELETE, UPDATE ON GestorFilialLoja TO Administrador;
+-- REVOKE SELECT, INSERT, DELETE, UPDATE ON AuxTGestorFilialLoja FROM Administrador;
+GRANT SELECT, INSERT, DELETE, UPDATE ON AuxTGestorFilialLoja TO Administrador;
 
 
 -- DROP VIEW VeiculosLojaGestorFilial
-CREATE OR REPLACE VIEW VeiculosLojaGestorFilial AS
+CREATE OR REPLACE VIEW ViVeiculosLojaGestorFilial AS
 SELECT *
 FROM Veiculo AS V
-NATURAL JOIN GestorFilialLoja AS G
+NATURAL JOIN AuxTGestorFilialLoja AS G
 	WHERE CURRENT_USER() = G.nome_completo  -- CURRENT_USER é o nome do utilizador atual
 		  AND V.id_loja = G.id_loja;
           
           
 -- DROP VIEW FuncionariosLojaGestorFilial
-CREATE OR REPLACE VIEW FuncionariosLojaGestorFilial AS
+CREATE OR REPLACE VIEW ViFuncionariosLojaGestorFilial AS
 SELECT *
 FROM Funcionario AS F
-NATURAL JOIN GestorFilialLoja AS G
+NATURAL JOIN AuxTGestorFilialLoja AS G
 	WHERE CURRENT_USER() = G.nome_completo  -- CURRENT_USER é o nome do utilizador atual
 		  AND F.id_loja = G.id_loja;         
           
           
 -- DROP VIEW LojaGestorFilial
-CREATE OR REPLACE VIEW LojaGestorFilial AS
+CREATE OR REPLACE VIEW ViLojaGestorFilial AS
 SELECT *
 FROM Loja AS L
-NATURAL JOIN GestorFilialLoja AS G
+NATURAL JOIN AuxTGestorFilialLoja AS G
 	WHERE CURRENT_USER() = G.nome_completo  -- CURRENT_USER é o nome do utilizador atual
-		  AND L.id_loja = G.id_loja;               
+		  AND L.id_loja = G.id_loja;    
+          
 
+-- DROP PROCEDURE PrUpdateVeiculos
+DELIMITER $$
+CREATE PROCEDURE PrUpdateVeiculos
+(
+  IN id_veiculo_a_atualizar INT,
+  IN matricula_nova VARCHAR(6),
+  IN estado_novo_veiculo ENUM('ALUGADO', 'LIVRE', 'MANUTENCAO'),
+  IN marca_nova VARCHAR(100),
+  IN tipo_veiculo_novo VARCHAR(200),
+  IN ano_novo INT
+) 
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		ROLLBACK;
+        SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'Erro a atualizar os dados do veículo';
+    END;
+    
+    START TRANSACTION;
+    procedimento: BEGIN
+		IF id_veiculo_a_atualizar IS NULL THEN
+			SIGNAL SQLSTATE '45002' SET MESSAGE_TEXT = 'Não é possivel atualizar os dados de um veículo pois não foi fornecido um ID';
+			LEAVE procedimento;
+		END IF;
+		IF matricula_nova IS NOT NULL THEN
+			UPDATE ViVeiculosLoja
+            SET matricula = matricula_nova
+            WHERE id_veiculo = id_veiculo_a_atualizar;
+        END IF;        
+		IF estado_novo_veiculo IS NOT NULL THEN
+			UPDATE ViVeiculosLoja
+            SET estado = estado_novo_veiculo
+            WHERE id_veiculo = id_veiculo_a_atualizar;
+        END IF;
+		IF marca_nova IS NOT NULL THEN
+			UPDATE ViVeiculosLoja
+            SET marca = marca_nova
+            WHERE id_veiculo = id_veiculo_a_atualizar;
+        END IF;
+		IF tipo_veiculo_novo IS NOT NULL THEN
+			UPDATE ViVeiculosLoja
+            SET tipo_veiculo = tipo_veiculo_novo
+            WHERE id_veiculo = id_veiculo_a_atualizar;
+        END IF;
+		IF ano_novo IS NOT NULL THEN
+			UPDATE ViVeiculosLoja
+            SET ano = ano_novo
+            WHERE id_veiculo = id_veiculo_a_atualizar;
+        END IF;
+    COMMIT;
+    END procedimento;
+END$$
+DELIMITER ;
+
+-- REVOKE EXECUTE ON PrUpdateVeiculos FROM GestorFilial;
+GRANT EXECUTE ON PrUpdateVeiculos TO GestorFilial;
 
 -- REVOKE SELECT, INSERT, DELETE, UPDATE ON VeiculosLojaGestorFilial FROM GestorFilial;
-GRANT SELECT, INSERT, DELETE, UPDATE ON VeiculosLojaGestorFilial TO GestorFilial;
+GRANT SELECT, INSERT, DELETE ON ViVeiculosLojaGestorFilial TO GestorFilial;
 -- REVOKE SELECT, INSERT, DELETE, UPDATE ON FuncionariosLojaGestorFilial FROM GestorFilial;
-GRANT SELECT, INSERT, DELETE, UPDATE ON FuncionariosLojaGestorFilial TO GestorFilial;
+GRANT SELECT, INSERT, DELETE, UPDATE ON ViFuncionariosLojaGestorFilial TO GestorFilial;
 -- REVOKE SELECT, INSERT, DELETE, UPDATE ON LojaGestorFilial FROM GestorFilial;
-GRANT SELECT, INSERT, DELETE, UPDATE ON LojaGestorFilial TO GestorFilial;
--- REVOKE SELECT, INSERT, DELETE, UPDATE ON Cliente FROM Administrador
-GRANT SELECT, INSERT, DELETE, UPDATE ON Cliente TO Administrador;
--- REVOKE SELECT, INSERT, UPDATE ON Aluguer FROM Administrador
-GRANT SELECT, INSERT, UPDATE ON Aluguer TO Administrador;
+GRANT SELECT, INSERT, DELETE, UPDATE ON ViLojaGestorFilial TO GestorFilial;
+-- REVOKE SELECT, INSERT, DELETE, UPDATE ON Cliente FROM GestorFilial
+GRANT SELECT, INSERT, DELETE, UPDATE ON Cliente TO GestorFilial;
+-- REVOKE SELECT, INSERT, UPDATE ON Aluguer FROM GestorFilial
+GRANT SELECT, INSERT, UPDATE ON Aluguer TO GestorFilial;
 
 -- REVOKE Funcionario FROM GestorFilial WITH ADMIN OPTION;
 GRANT Funcionario TO GestorFilial WITH ADMIN OPTION;
+
+-- REVOKE GestorFilial FROM 'filipa_coelho'@'localhost'
+GRANT GestorFilial TO 'filipa_coelho'@'localhost';
+-- REVOKE GestorFilial FROM 'jorge_coelho'@'localhost'
+GRANT GestorFilial TO 'jorge_coelho'@'localhost';
+
+-- DELETE FROM AuxTGestorFilialLoja WHERE nome_completo = 'filipa_coelho@localhost'
+-- DELETE FROM AuxTGestorFilialLoja WHERE nome_completo = 'jorge_coelho@localhost'
+INSERT INTO AuxTGestorFilialLoja VALUES
+('filipa_coelho@localhost', 2),
+('jorge_coelho@localhost', 3);
 
 
 -- DROP ROLE Funcionario
