4fc2d83f5f4e4179a82c85709ec9020bbd336d9f
a107365 <a107365@alunos.uminho.pt>
2025-05-30 18:20:03 +0100

    dois clientes

--- Populacao.sql
+++ Populacao.sql
@@ -14,3 +14,8 @@ INSERT INTO Loja(email, cidade, rua, numero_porta, codigo_postal) VALUES
 INSERT INTO Veiculo(matricula, preco, estado, marca, tipo_veiculo, ano, id_loja) VALUES
 ('16BA23', 800, 'LIVRE', 'CAT', 'Escavadora', 2017, 2),
 ('25QI03', 550, 'ALUGADO', 'Betumix', 'Betuneira', 2021, 3);
+
+
+INSERT INTO Cliente(numero_telemovel, empresa, nome_completo, cidade, rua, numero_porta, codigo_postal, numero_carta, data_validade_carta, nif, numero_documento, data_validade_documento) VALUES
+(912345678, 0, 'João Silva', 'Braga', 'Rua das Flores', 10, '4700-000', 12345678, '2028-05-01', 123456789, 987654321, '2029-12-31'),
+(913456789, 1, 'Empresa XPTO Lda', 'Porto', 'Avenida Central', 101, '4000-123', NULL, NULL, 987654321, 123123123, '2030-01-01');
